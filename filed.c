0838e97bf1c137f286044ce6b526b42675a393fa
rkeene <rkeene@noemail.net>
2014-02-08 06:59:07 +0000

    Post-release version increment
    FossilOrigin-Name: 525e4358e2a526b5d6d44fa0c5daf43ed1c62bc0

--- filed.c
+++ filed.c
@@ -20,7 +20,7 @@
 #include <pwd.h>
 
 /* Compile time constants */
-#define FILED_VERSION "1.0"
+#define FILED_VERSION "1.1"
 #define FILED_SENDFILE_MAX 16777215
 #define FILED_MAX_FAILURE_COUNT 30
 #define FILED_DEFAULT_TYPE "application/octet-stream"
