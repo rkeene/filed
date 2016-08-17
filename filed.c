c033550da52e7694cb59aa94e72c2e3356869c2d
rkeene <rkeene@noemail.net>
2016-08-17 16:22:41 +0000

    Filed 1.18
    FossilOrigin-Name: a78878e047f5b55f0851e21697974fe3b3e42e56

--- filed.c
+++ filed.c
@@ -69,7 +69,7 @@
 #endif
 
 /* Compile time constants */
-#define FILED_VERSION "1.17"
+#define FILED_VERSION "1.18"
 #define FILED_SENDFILE_MAX 16777215
 #define FILED_MAX_FAILURE_COUNT 30
 #define FILED_DEFAULT_TYPE "application/octet-stream"
